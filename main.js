18d11e3e46bf62d982fdcf6741b8ae7decee2b22
nasuda <nasuda908@gmail.com>
2023-08-12 22:38:04 +0900

    add fragment

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const root = document.getElementById('root');
 const title = 'It is root';
 root.innerHTML = `<h1>${title}</h1>`;
+
+const ul = document.createElement('ul');
+const abcd = ['a', 'b', 'c', 'd'];
+
+const fragment = new DocumentFragment();
+
+for (const str of abcd) {
+  const li = document.createElement('li');
+  li.textContent = str;
+  fragment.appendChild(li);
+}
+
+root.appendChild(ul);
+ul.appendChild(fragment);
